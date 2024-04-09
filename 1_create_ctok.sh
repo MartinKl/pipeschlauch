9bffe307d46bcfc0ef3af18ee10fff1ad9aa5caf
Martin Klotz <martin.klotz@hu-berlin.de>
2024-04-09 14:59:32 +0200

    updated pipeline, added check step

--- 1_create_ctok.sh
+++ 1_create_ctok.sh
@@ -1,3 +1,3 @@
-mkdir data/0
-mkdir data/0/SeiKo/
-python create_ctok.py data/original/ data/0/SeiKo/
+mkdir data/1
+mkdir data/1/SeiKo/
+python create_ctok.py data/original/ data/1/SeiKo/
--- 3_tag.sh
+++ 3_tag.sh
@@ -1,6 +1,6 @@
 cd tools/treetagger
-mkdir ../../data/2/
-mkdir ../../data/2/SeiKo
-for f in ../../data/1/SeiKo/*.tt; do
-	bin/tree-tagger lib/german-spoken-stts2.par -token -sgml -lemma -no-unknown -eos-tag unit ${f} > ../../data/2/SeiKo/$(basename $f)
+mkdir ../../data/3/
+mkdir ../../data/3/SeiKo
+for f in ../../data/2/SeiKo/*.tt; do
+	bin/tree-tagger lib/german-spoken-stts2.par -token -sgml -lemma -no-unknown -eos-tag unit ${f} > ../../data/3/SeiKo/$(basename $f)
 done
