04a05b4f5089d5661ef8a50096cebf1150bd3232
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 11:38:02 +0100

    only try to create non-existing folders

--- 3_tag.sh
+++ 3_tag.sh
@@ -1,6 +1,6 @@
 cd tools/treetagger
-mkdir ../../data/3/
-mkdir ../../data/3/SeiKo
+[ ! -d ../../data/3/ ] && mkdir ../../data/3/
+[ ! -d ../../data/3/SeiKo ] && mkdir ../../data/3/SeiKo
 for f in ../../data/2/SeiKo/*.tt; do
 	bin/tree-tagger lib/german-spoken-stts2.par -token -sgml -lemma -no-unknown -eos-tag unit ${f} > ../../data/3/SeiKo/$(basename $f)
 done
