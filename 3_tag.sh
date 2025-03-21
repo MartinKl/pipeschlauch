ce96c6b65e39fd032f1a5e22ffe12517ac9da5f4
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 12:12:21 +0100

    fixed bug

--- 3_tag.sh
+++ 3_tag.sh
@@ -1,5 +1,5 @@
 cd tools/treetagger
-[ ! -d ../../data/3/ ] && mkdir ../../data/3-pos/
+[ ! -d ../../data/3/ ] && mkdir ../../data/3/
 [ ! -d ../../data/3/SeiKo-pos ] && mkdir ../../data/3/SeiKo-pos
 [ ! -d ../../data/3/SeiKo-lemma ] && mkdir ../../data/3/SeiKo-lemma
 for f in ../../data/2/SeiKo/*.txt; do
