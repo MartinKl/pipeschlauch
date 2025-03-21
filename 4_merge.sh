b770e8c468e556b706ce87f65c68fa15f4fd7677
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 14:29:14 +0100

    use annatto

--- 4_merge.sh
+++ 4_merge.sh
@@ -1,7 +1,5 @@
-cd tools/pepper/
-./pepper.sh ../../merge2exb.pepper
-for f in ../../data/4/SeiKo/*_seg.exb
+annatto run merge2exb.toml
+for f in data/4/SeiKo/*_seg.exb
 do
-	echo $f
 	mv $f $(dirname $f)/$(basename -s _seg.exb $f)_pos.exb
 done
