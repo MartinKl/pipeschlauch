16835a90efa506dd068e66c1c1cb23f86c10451e
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 11:39:40 +0100

    use correct file ending

--- 3_tag.sh
+++ 3_tag.sh
@@ -1,6 +1,6 @@
 cd tools/treetagger
 [ ! -d ../../data/3/ ] && mkdir ../../data/3/
 [ ! -d ../../data/3/SeiKo ] && mkdir ../../data/3/SeiKo
-for f in ../../data/2/SeiKo/*.tt; do
+for f in ../../data/2/SeiKo/*.txt; do
 	bin/tree-tagger lib/german-spoken-stts2.par -token -sgml -lemma -no-unknown -eos-tag unit ${f} > ../../data/3/SeiKo/$(basename $f)
 done
