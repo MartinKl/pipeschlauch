2b0e72ba492a79f54c04478f045e8ad356acd276
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 12:08:33 +0100

    tag pos and lemma with separate parameter files

--- 3_tag.sh
+++ 3_tag.sh
@@ -1,6 +1,9 @@
 cd tools/treetagger
-[ ! -d ../../data/3/ ] && mkdir ../../data/3/
-[ ! -d ../../data/3/SeiKo ] && mkdir ../../data/3/SeiKo
+[ ! -d ../../data/3-pos/ ] && mkdir ../../data/3-pos/
+[ ! -d ../../data/3-pos/SeiKo ] && mkdir ../../data/3-pos/SeiKo
+[ ! -d ../../data/3-lemma/ ] && mkdir ../../data/3-lemma/
+[ ! -d ../../data/3-lemma/SeiKo ] && mkdir ../../data/3-lemma/SeiKo
 for f in ../../data/2/SeiKo/*.txt; do
-	bin/tree-tagger lib/german-spoken-stts2.par -token -sgml -lemma -no-unknown -eos-tag unit ${f} > ../../data/3/SeiKo/$(basename $f)
+	bin/tree-tagger lib/german-spoken-stts2.par -token ${f} > ../../data/3-pos/SeiKo/$(basename $f)
+	bin/tree-tagger lib/german.par -token -lemma ${f} > ../../data/3-lemma/SeiKo/$(basename $f)
 done
