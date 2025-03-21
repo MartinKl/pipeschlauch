6b77fb7370de4c2c706579bb34a26ea28b94909c
Martin Klotz <martin.klotz@hu-berlin.de>
2025-03-21 12:11:36 +0100

    use different folder structure

--- 3_tag.sh
+++ 3_tag.sh
@@ -1,9 +1,8 @@
 cd tools/treetagger
-[ ! -d ../../data/3-pos/ ] && mkdir ../../data/3-pos/
-[ ! -d ../../data/3-pos/SeiKo ] && mkdir ../../data/3-pos/SeiKo
-[ ! -d ../../data/3-lemma/ ] && mkdir ../../data/3-lemma/
-[ ! -d ../../data/3-lemma/SeiKo ] && mkdir ../../data/3-lemma/SeiKo
+[ ! -d ../../data/3/ ] && mkdir ../../data/3-pos/
+[ ! -d ../../data/3/SeiKo-pos ] && mkdir ../../data/3/SeiKo-pos
+[ ! -d ../../data/3/SeiKo-lemma ] && mkdir ../../data/3/SeiKo-lemma
 for f in ../../data/2/SeiKo/*.txt; do
-	bin/tree-tagger lib/german-spoken-stts2.par -token ${f} > ../../data/3-pos/SeiKo/$(basename $f)
-	bin/tree-tagger lib/german.par -token -lemma ${f} > ../../data/3-lemma/SeiKo/$(basename $f)
+	bin/tree-tagger lib/german-spoken-stts2.par -token ${f} > ../../data/3/SeiKo-pos/$(basename $f)
+	bin/tree-tagger lib/german.par -token -lemma ${f} > ../../data/3/SeiKo-lemma/$(basename $f)
 done
